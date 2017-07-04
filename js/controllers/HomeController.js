4d21ce8c73f1b3f37edabbdd9a88acaa5a75ddf7
Behnam Nikkhah <bnikkhah22@gmail.com>
2017-07-04 14:14:21 -0600

    suggest box

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,4 +1,20 @@
 app.controller("HomeController", ["$scope", "suggestions", 
 	function($scope, suggestions){
 	$scope.posts = suggestions.posts;
+	
+	$scope.addSuggestion = function(){
+		//if input empty, don't submit
+		if (!$scope.title || $scope.title === ""){
+			return;
+		}
+		
+		//push suggestion posts in suggestions.js
+		$scope.posts.unshift({
+			title: $scope.title,
+			upvotes: 0,
+		});
+		
+		//after submit, clear input
+		$scope.title = "";
+	}
 }]);
