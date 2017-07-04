e69caae5cfada6c4dd2116b28e9d3b0ad799dbe0
Behnam Nikkhah <bnikkhah22@gmail.com>
2017-07-04 14:37:16 -0600

    upvoting

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -17,4 +17,8 @@ app.controller("HomeController", ["$scope", "suggestions",
 		//after submit, clear input
 		$scope.title = "";
 	}
+	
+	$scope.upVote = function(post){
+		post.upvotes += 1;
+	}
 }]);
