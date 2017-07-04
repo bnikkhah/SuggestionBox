a2c7c7a8034ddf35f38e566c8ae9bca68a8fe859
Behnam Nikkhah <bnikkhah22@gmail.com>
2017-07-04 17:30:07 -0600

    comments section

--- js/controllers/SuggestionController.js
+++ js/controllers/SuggestionController.js
@@ -19,29 +19,28 @@
 	};
 }]);*/
 
-/*app.controller('SuggestionController', ['$scope', '$routeParams',
+app.controller('SuggestionController', ['$scope', '$routeParams',
  'suggestions', function($scope, $routeParams, suggestions) {
 
     $scope.post = suggestions.posts[$routeParams.id];
+	
     $scope.addComment = function(){
-        if(!$scope.my_comments || $scope.my_comments === '' ){
+        if(!$scope.myComments || $scope.myComments === '' ){
             return;
         }
         $scope.post.comments.push({
-            body:$scope.my_comments,
+            body:$scope.myComments,
             upvotes:0,
         });
-        $scope.my_comments = '';
+        $scope.myComments = '';
     };
-    $scope.remove = function($index){
+	
+    /*$scope.remove = function($index){
         $scope.post.comments.splice($index, 1);
-    };
+    };*/
+	
     $scope.upvoteComments = function(comment){
         comment.upvotes += 1;
     };
 
- }]);*/
- 
- app.controller("SuggestionController", ["$scope", "suggestions", "$routeParams", function($scope, suggestions, $routeParams){
-	 
  }]);
