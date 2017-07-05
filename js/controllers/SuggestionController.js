f16775714de61c9854108a56ca3261abcbe48c69
Behnam Nikkhah <bnikkhah22@gmail.com>
2017-07-04 21:01:53 -0600

    comment section

--- js/controllers/SuggestionController.js
+++ js/controllers/SuggestionController.js
@@ -1,43 +1,19 @@
-/*app.controller("SuggestionController", ["$scope", "$routeParams", "suggestions", function($scope, $routeParams, suggestions){
-	$scope.post = suggestions.posts[$routeParams.id];
-	
-	$scope.addComment = function(){
-		if (!$scope.my_comments || $scope.my_comments === ""){
-			return;
-		}
-		
-		$scope.post.comments.push({
-			body:$scope.my_comments,
-			upvotes:0
-		});
-		
-		$scope.my_comments = "";
-	};
-	
-	$scope.upvoteComments = function(comment){
-		comment.upvotes += 1;
-	};
-}]);*/
-
-app.controller('SuggestionController', ['$scope', '$routeParams',
- 'suggestions', function($scope, $routeParams, suggestions) {
+app.controller("SuggestionController", ["$scope", "$routeParams", "suggestions", function($scope, $routeParams, suggestions) {
 
     $scope.post = suggestions.posts[$routeParams.id];
 	
     $scope.addComment = function(){
-        if(!$scope.myComments || $scope.myComments === '' ){
+        if(!$scope.myComments || $scope.myComments === "" ){
             return;
         }
+		
         $scope.post.comments.push({
             body:$scope.myComments,
             upvotes:0,
         });
-        $scope.myComments = '';
+		
+        $scope.myComments = "";
     };
-	
-    /*$scope.remove = function($index){
-        $scope.post.comments.splice($index, 1);
-    };*/
 	
     $scope.upvoteComments = function(comment){
         comment.upvotes += 1;
